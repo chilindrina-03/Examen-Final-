112d3ac3b45ffa06d112a13f6fce1a549fbda6b2
chilindrina-03 <66022553+chilindrina-03@users.noreply.github.com>
2020-05-31 13:15:51 -0500

    Adición de archivos
    se adicionan los archivos,  de ejercicio 1,  resultados ejercicio 1 y archivo de texto,   que explican el proceso para el desarrollo del problema propuesto.

---
+++ ejercicio 1 py..py
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+nombre=input("rol")
+cedula=input("numero de cedula")
+producto=int(input("valor del producto $ "))
+resultadodescuento=producto*20/100
+resultadototal=producto-resultadodescuento
+codigo=int(input("numero codigo"))
+descuento=int(input("valor del descuento %"))
+print ("el",nombre,"con cedula",cedula,"debe pagar",producto,"por el codigo",codigo,"descuento aplicado",resultadodescuento,"total a pagar",resultadototal)
+
+
+
+nombre=input("rol")
+cedula=input("numero de cedula")
+producto=int(input("valor del producto $ "))
+resultadodescuento=producto*20/100
+resultadototal=producto-resultadodescuento
+codigo=int(input("numero codigo"))
+descuento=int(input("valor del descuento %"))
+print ("el",nombre,"con cedula",cedula,"debe pagar",producto,"por el codigo",codigo,"descuento aplicado",resultadodescuento,"total a pagar",resultadototal)
---
+++ resultado ejercicio 1 py..py
@@ -0,0 +1,17 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: D:/ARQUITECTURA CARRERA/5to SEMESTRE/HERRAMIENTAS COMPUTACIONALES/examen/ejercicio 1 py..py
+rolprofesor 
+numero de cedula 1454898
+valor del producto $ 12900
+numero codigo076
+valor del descuento %20
+el profesor  con cedula  1454898 debe pagar 12900 por el codigo 76 descuento aplicado 2580.0 total a pagar 10320.0
+rolestudiante 
+numero de cedula1053775583
+valor del producto $ 12900
+numero codigo076
+valor del descuento %40
+el estudiante  con cedula 1053775583 debe pagar 12900 por el codigo 76 descuento aplicado 2580.0 total a pagar 10320.0
+>>> 
